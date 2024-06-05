32a6428848253cb8f813b94ccc2e6f974100ef58
Jordan Medina Ortiz <120710792+JordanMedinaOrtiz@users.noreply.github.com>
2024-06-04 19:35:09 -0600

    Add files via upload

---
+++ src/scripts/data/articles.js
@@ -0,0 +1,45 @@
+//Simulamos base de datos
+export default {
+    articles: [
+        {
+            id: '1',
+            nombre: 'Halo Infinite',
+            price: 999,
+        },
+        {
+            id: '2',
+            nombre: 'F1 24',
+            price: 1099,
+        },
+        {
+            id: '3',
+            nombre: 'Assassins Creed Shadows Gold Edition',
+            price: 1999,
+        },
+        {
+            id: '4',
+            nombre: 'Little Nightmares III',
+            price: 899,
+        },
+        {
+            id: '5',
+            nombre: 'Silent Hill 2',
+            price: 1299,
+        },
+        {
+            id: '6',
+            nombre: 'Metal Gear Solid Delta Snake Eater',
+            price: 1299,
+        },
+        {
+            id: '7',
+            nombre: 'Grand Theft Auto V',
+            price: 999,
+        },
+        {
+            id: '8',
+            nombre: 'Dragon Ball Sparking Zero',
+            price: 1199,
+        },
+    ]
+}
